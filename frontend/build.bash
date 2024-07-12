1a7e65ed52dae64d19d9fd80add17c85d5b00153
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-07-11 22:51:00 -0400

    Integrated the .ico script into the build pipeline

--- frontend/build.bash
+++ frontend/build.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 ## Builds a webapp
 
-## Paths
+## Paths and variables
 export \
     SRC_DIR="$PWD/src" \
     DIST_DIR="$PWD/dist" \
-    NODE_BIN="$PWD/node_modules/.bin"
+    NODE_BIN="$PWD/node_modules/.bin" \
+    CSS_LIST=$(mktemp)
 
 ## Wipe the old `$DIST_DIR`
 if [[ -d "$DIST_DIR" ]]; then
@@ -17,8 +18,32 @@ if [[ -d "$DIST_DIR" ]]; then
 fi
 mkdir -p "$DIST_DIR"
 
+## Define a function for each filetype
+function do-ext {
+    eval "$3" "$1" "$2"
+    if [[ $? -eq 0 ]]; then
+        echo "'$1' > '$2'"
+    else
+        echo "Error on '$1' > '$2'" >&2
+    fi
+}
+function do-css {
+    echo "$1" >> "$CSS_LIST" && \
+    echo "'$1' >> '$DIST_DIR/styles.css'"
+}
+function xml {
+    "$NODE_BIN/minify-xml" "$1" > "$2"
+}
+function svg {
+    xml "$1" "$2" && \
+    ./svg2ico.sh "$2" "${2%.*}.ico"
+    # echo "'$1' > '${2%.*}.ico'"
+}
+function xxx {
+    cat "$1" > "$2"
+}
+
 ## Do the thing
-declare -a CSS_FILES=()
 while IFS= read -r -d $'\0' IN; do
     OUT="${IN/#$SRC_DIR/$DIST_DIR}"
 
@@ -27,15 +52,17 @@ while IFS= read -r -d $'\0' IN; do
 
     function do-cat { cat "$IN" > "$OUT"; }
     case "${IN##*.}" in
-        'css') CSS_FILES+=("$IN") && echo "$IN >> $DIST_DIR/styles.css" && continue ;;
-        'xml'|'xhtml'|'svg') "$NODE_BIN/minify-xml" "$IN" > "$OUT" & echo "$IN > $OUT" && continue;;
-        *) do-cat & echo "$IN > $OUT" && continue ;;
+        'css')         do-css "$IN" "$OUT"     & continue ;;
+        'xml'|'xhtml') do-ext "$IN" "$OUT" xml & continue ;;
+        'svg')         do-ext "$IN" "$OUT" svg & continue ;;
+        *)             do-ext "$IN" "$OUT" xxx & continue ;;
     esac
 done < <(find "$SRC_DIR" \( -type f -o -type l \) -print0)
 wait
 
 ## Concatenate and optimize CSS
-"$NODE_BIN/cleancss" -o "$DIST_DIR/styles.css" $(echo "${CSS_FILES[@]}" | sort | xargs)
+"$NODE_BIN/cleancss" -o "$DIST_DIR/styles.css" $(cat "$CSS_LIST" | sort | xargs)
+rm "$CSS_LIST"
 
 ## Done
 rmdir "$DIST_DIR/"* 2>/dev/null #TODO: Delete all empty directories recursively, starting at the deepest ones.
